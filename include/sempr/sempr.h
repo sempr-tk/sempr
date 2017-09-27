e6f840ce4362ce27505eb3746e490b15770ba68a
Nils Niemann <nils.niemann@dfki.de>
2017-09-27 10:17:12 +0200

    updated sempr.h

--- include/sempr/sempr.h
+++ include/sempr/sempr.h
@@ -1,12 +1,21 @@
 // core
 #include <sempr/core/Core.h>
 #include <sempr/core/Event.h>
+#include <sempr/core/EntityEvent.h>
+#include <sempr/core/EventBroker.h>
+#include <sempr/core/Observer.h>
+#include <sempr/core/RDF.h>
 
-// data
-#include <sempr/data/Data.h>
+// entity
+#include <sempr/entity/Entity.h>
+
+// processing
+#include <sempr/processing/Module.h>
+
+// query
 
 // storage
-#include <sempr/storage/Storage.h>
-#include <sempr/storage/ODBStorage.h>
 #include <sempr/storage/DBObject.h>
 #include <sempr/storage/History.h>
+#include <sempr/storage/ODBStorage.h>
+#include <sempr/storage/Storage.h>
