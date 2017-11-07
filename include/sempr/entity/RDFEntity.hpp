574af8fe1a0fd8356875dd39db8f10f4afee9592
Nils Niemann <nils.niemann@dfki.de>
2017-11-07 12:07:14 +0100

    RDFEntity from interface to full entity.

--- include/sempr/entity/RDFEntity.hpp
+++ include/sempr/entity/RDFEntity.hpp
@@ -1,20 +1,36 @@
 #ifndef SEMPR_ENTITY_RDFENTITY_H_
 #define SEMPR_ENTITY_RDFENTITY_H_
 
+#include <odb/core.hxx>
 #include <sempr/core/RDF.hpp>
 #include <sempr/entity/Entity.hpp>
-#include <sempr/core/EventBroker.hpp>
+#include <sempr/entity/Triple.hpp>
+
+// #include <sempr/core/EventBroker.hpp>
 #include <sempr/core/EntityEvent.hpp>
+
+#include <vector>
 
 namespace sempr { namespace entity {
 
 #pragma db object
 class RDFEntity : public Entity {
 public:
+    using Ptr = std::shared_ptr<RDFEntity>;
     using Event = core::EntityEvent<RDFEntity>;
-    virtual void getTriples(std::vector<core::Triple>& triples) const = 0;
-    
+    virtual ~RDFEntity(){}
+
+    void getTriples(std::vector<Triple>& triples) const;
+    void addTriple(const Triple& triple);
+    void removeTriple(const Triple& triple);
+    void clear();
+
+
     ENTITY_DEFAULT_EVENT_METHODS(RDFEntity, Entity);
+
+protected:
+    friend class odb::access;
+    std::vector<Triple> triples_;
 };
 
 }}
---
+++ src/entity/RDFEntity.cpp
@@ -0,0 +1,32 @@
+#include <sempr/entity/RDFEntity.hpp>
+// #include <RDFEntity_odb.h>
+
+namespace sempr { namespace entity {
+
+void RDFEntity::getTriples(std::vector<Triple> &triples) const
+{
+    triples.insert(triples.end(), triples_.begin(), triples_.end());
+}
+
+void RDFEntity::addTriple(const sempr::entity::Triple &triple)
+{
+    triples_.push_back(triple);
+    this->changed();
+}
+
+void RDFEntity::removeTriple(const sempr::entity::Triple &triple)
+{
+    auto newEnd = std::remove(triples_.begin(), triples_.end(), triple);
+    triples_.erase(newEnd);
+    this->changed();
+}
+
+void RDFEntity::clear()
+{
+    triples_.clear();
+    this->changed();
+}
+
+
+} /* entity */
+} /* sempr */
