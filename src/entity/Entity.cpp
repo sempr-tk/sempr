4799ca74143b2fd1c43f24846e84950941091596
Nils Niemann <nils.niemann@dfki.de>
2017-11-08 11:04:29 +0100

    Entity: register children and cope with them on persist/update

--- src/entity/Entity.cpp
+++ src/entity/Entity.cpp
@@ -76,6 +76,84 @@ void Entity::removed_impl() {
     baseCalled_ = true;
 }
 
+
+
+void Entity::registerChildEntity(Entity::Ptr child)
+{
+    children_.push_back(child);
+}
+
+
+void Entity::prePersist(odb::database &db) const
+{
+    handleChildrenPre(db);
+}
+
+void Entity::postPersist(odb::database &db) const
+{
+    handleChildrenPost(db);
+}
+
+void Entity::preUpdate(odb::database &db) const
+{
+    handleChildrenPre(db);
+}
+
+void Entity::postUpdate(odb::database &db) const
+{
+    handleChildrenPost(db);
+}
+
+
+void Entity::handleChildrenPre(odb::database &db) const
+{
+    std::cout << discriminator() << "  PRE" << '\n';
+    for (auto child : children_)
+    {
+        // set the broker first, so that the child may fire events after it has
+        // been persisted. This needs to be done now, because the child may have
+        // children itself that it wants to fire events for in the cause of
+        // its "persist".
+        child->broker_ = broker_;
+        db.persist(child);
+    }
+}
+
+void Entity::handleChildrenPost(odb::database &db) const
+{
+    std::cout << discriminator() << "  POST" << '\n';
+
+    // set the childrens parent to this.
+    for (auto child : children_)
+    {
+        child->setParent(shared_from_this());
+        db.update(child);
+    }
+    // persist first, process second
+    for (auto child : children_)
+    {
+        child->created();
+    }
+
+    // no need to do this ever again:
+    children_.clear();
+
+    /*
+        Cite from the documentation (p. 223):
+        > If you need to modify the object in one of the "const" events, then
+        > you can safely cast away const-ness using the const_cast operator if
+        > you know that none of the objects will be created const.
+
+        Could be useful someday. Right now, I just made children_ mutable and
+        the parent_-pointer point to a const DBObject. We won't need to modify
+        the parent through that pointer, I guess, it's just there for the
+        on-delete-cascade. But, if we eventually do, just make it non-const again
+        and cast away the const-ness in child->setParent using
+        std::const_pointer_cast<DBObject>(shared_from_this()).
+    */
+}
+
+
 } /* entity */
 
 } /* sempr */
