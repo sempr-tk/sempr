46ef541737c2601834d1f86687467fb8b32f0b20
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-11-13 15:11:02 +0100

    fixed test failure due to double object creation

--- test/SEMPR_tests.cpp
+++ test/SEMPR_tests.cpp
@@ -25,17 +25,16 @@ BOOST_AUTO_TEST_SUITE(general_tests)
     Core core(storage);
     core.addModule(active);
     core.addModule(updater);
-
-    // TODO: write real test cases.
   }
 
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(entity_within_entity_tests)
 
+boost::uuids::uuid id;
+
 BOOST_AUTO_TEST_CASE(insertion)
 {
-
   // insertion
   {
     // setup. database will be reset (true)
@@ -46,44 +45,44 @@ BOOST_AUTO_TEST_CASE(insertion)
     core.addModule(updater);
 
     // create  a person, which creates an rdf-entity within
-    Person::Ptr p(new Person());
-    // save --> persist the person. check 1: we should not get a foreign-key-
-    // constraint-violation if the rdf-entity is persisted before the person.
-    core.addEntity(p);
-  }
-}
-
-BOOST_AUTO_TEST_CASE(retrieval)
-{
-    // retrieval. New ODBStorage, new session, force to load from database
-  {
-    ODBStorage::Ptr storage( new ODBStorage("test_sqlite.db") );
-    DBUpdateModule::Ptr updater( new DBUpdateModule(storage) );
-    DebugModule::Ptr debug( new DebugModule() );
-
-    Core core(storage);
-    core.addModule(updater);
-    core.addModule(debug);
-
-    // create  a person, which creates an rdf-entity within
     Person::Ptr person(new Person());
     // save --> persist the person. check 1: we should not get a foreign-key-
     // constraint-violation if the rdf-entity is persisted before the person.
     core.addEntity(person);
 
-    boost::uuids::uuid id = person->uuid();
-
-    DBObject::Ptr p = storage->load(id);
-
-
-    // check if the object is of correct type by using the discriminator.
-    BOOST_CHECK_EQUAL(p->discriminator(), DISCRIMINATOR(Person));
-
-    // load all entites, check if there are exactly 2 (person and its RDF).
-    std::vector<DBObject::Ptr> all;
-    storage->loadAll(all);
-    BOOST_CHECK_EQUAL(all.size(), 2);
+    id = person->uuid();
   }
+}
+
+BOOST_AUTO_TEST_CASE(retrieval){
+    // retrieval. New ODBStorage, new session, force to load from database
+
+  ODBStorage::Ptr storage( new ODBStorage("test_sqlite.db") );
+  DBUpdateModule::Ptr updater( new DBUpdateModule(storage) );
+  DebugModule::Ptr debug( new DebugModule() );
+
+  Core core(storage);
+  core.addModule(updater);
+  core.addModule(debug);
+
+  DBObject::Ptr p = storage->load(id);
+
+  // check if the object is of correct type by using the discriminator.
+  BOOST_CHECK_EQUAL(p->discriminator(), DISCRIMINATOR(Person));
+
+  // load all entites, check if there are exactly 2 (person and its RDF).
+  std::vector<DBObject::Ptr> all;
+  storage->loadAll(all);
+  BOOST_CHECK_EQUAL(all.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(update) {
+
+  
+}
+
+BOOST_AUTO_TEST_CASE(deletion) {
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
