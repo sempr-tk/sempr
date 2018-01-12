eb3418df89a5c9138a76a3a681cb654b24051f08
Nils Niemann <nils.niemann@dfki.de>
2018-01-12 10:59:52 +0100

    added rdf:type to Person

--- src/entity/Person.cpp
+++ src/entity/Person.cpp
@@ -18,6 +18,10 @@ Person::Person(const core::IDGenBase* idgen)
     m["name"] = "Max Mustermann";
     m["age"] = 42;
     m["height"] = 1.85;
+
+    RDFEntity::Ptr basic(new RDFEntity());
+    basic->addTriple(Triple( "<" + sempr::baseURI() + this->id() + ">", sempr::core::rdf::type(), "<" + sempr::baseURI() + "Person>"));
+    registerChildEntity(basic);
 }
 
 Person::Person() : Person(new core::IDGen<Person>())
