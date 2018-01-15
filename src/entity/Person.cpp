fc4434e32e4cdc0050c691d511feabfd5b03c325
Nils Niemann <nils.niemann@dfki.de>
2018-01-15 12:09:58 +0100

    save persons rdf:type inside propertymap

--- src/entity/Person.cpp
+++ src/entity/Person.cpp
@@ -19,9 +19,10 @@ Person::Person(const core::IDGenBase* idgen)
     m["age"] = 42;
     m["height"] = 1.85;
 
-    RDFEntity::Ptr basic(new RDFEntity());
-    basic->addTriple(Triple( "<" + sempr::baseURI() + this->id() + ">", sempr::core::rdf::type(), "<" + sempr::baseURI() + "Person>"));
-    registerChildEntity(basic);
+    // RDFEntity::Ptr basic(new RDFEntity());
+    // basic->addTriple(Triple( "<" + sempr::baseURI() + this->id() + ">", sempr::core::rdf::type(), "<" + sempr::baseURI() + "Person>"));
+    // registerChildEntity(basic);
+    m("type", core::rdf::baseURI()) = RDFResource("<" + sempr::baseURI() + "Person" + ">");
 }
 
 Person::Person() : Person(new core::IDGen<Person>())
