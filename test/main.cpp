d42c58d8c89ed9551dd6b117d81fa0f2b1504507
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2018-01-26 14:02:42 +0100

    test skeleton

--- test/main.cpp
+++ test/main.cpp
@@ -21,125 +21,83 @@ using namespace sempr::query;
 
 #include <sempr/core/IncrementalIDGeneration.hpp>
 
-// small, easy, specialized query for persons over the age of 55.
-class PersonQuery : public ObjectQueryBase {
-public:
-    std::vector<Person::Ptr> results;
-    void consider(DBObject::Ptr o) override
-    {
-        auto ptr = std::dynamic_pointer_cast<Person>(o);
-        if (ptr)
-        {
-            if (ptr->age() >= 55) {
-                results.push_back(ptr);
-            }
-        }
-    }
-};
-
+#include <chrono>
 
 
 int main(int argc, char** args)
 {
-    int numInsert = 1;
-    ODBStorage::Ptr storage( new ODBStorage() );
+  int numInsert = 1;
+  bool inmemory = true;
 
-    if (argc > 1) numInsert = atoi(args[1]);
-    if (argc > 2) {
-        storage.reset();
-        storage.reset(new ODBStorage(":memory:"));
+  switch(argc){
+  case 2:{
+      std::cout << argc << std::endl;
+      numInsert = atoi(args[1]);
     }
-
-/* Performance results for "insert X persons, load them all, increment age"
-1. In-Memory-Database, X = 10000
-
-    time ./test/manual_test 10000 mem > /dev/null
-    real    0m3.994s
-    user    0m3.976s
-    sys     0m0.016s
-
-2. File-Database (sqlite3), X = 10000
-
-    time ./test/manual_test 10000 > /dev/null
-    real    13m6.421s
-    user    0m23.488s
-    sys     0m32.204s
-*/
-
-
-
-    // ODBStorage::Ptr storage( new ODBStorage(":memory:") );
-    // ODBStorage::Ptr storage( new ODBStorage() );
-    DebugModule::Ptr debug( new DebugModule() );
-    DBUpdateModule::Ptr updater( new DBUpdateModule(storage) );
-    ActiveObjectStore::Ptr active( new ActiveObjectStore() );
-
-    sempr::core::IDGenerator::getInstance().setStrategy(
-        // std::unique_ptr<sempr::core::UUIDGeneration>( new sempr::core::UUIDGeneration(false) )
-        std::unique_ptr<sempr::core::IncrementalIDGeneration>( new sempr::core::IncrementalIDGeneration(storage) )
-    );
-
-    sempr::core::Core c(storage);
-    c.addModule(active);
-    // c.addModule(debug);
-    c.addModule(updater);
-
-
+    break;
+  case 3:{
+      numInsert = atoi(args[1]);
+      inmemory = boost::lexical_cast<bool>(std::atoi(args[2]));
+    }
+    break;
+  default:
     {
-        // insert.
-        for (int i = 0; i < numInsert; i++) {
-            Person::Ptr p(new Person());
-            c.addEntity(p);
-        }
-        // Person::Ptr p2(new Person());
-        // c.addEntity(p2);
-
-
-        // retrieve
-
-        // std::vector<DBObject::Ptr> objects;
-        // storage->loadAll(objects);
-        // for (auto o : objects) {
-        //     Person::Ptr p = std::dynamic_pointer_cast<Person>(o);
-        //     if (p) {
-        //         p->loaded(); // no changed-events before announcement!
-        //         std::cout << "Person id: " << p->id() << std::endl;
-        //
-        //         // add a year.
-        //         p->age(p->age()+1);
-        //         std::cout << "Person: "
-        //             << p->name() << ", "
-        //             << p->age() << " years old, "
-        //             << p->height() << "m." << std::endl;
-        //     }
-        // }
-
-        std::vector<Person::Ptr> persons;
-        storage->loadAll(persons);
-        for (auto p : persons) {
-            p->loaded(); // no changed-events before announcement!
-            // std::cout << "Person id: " << p->id() << std::endl;
-
-            // add a year.
-            p->age(p->age()+1);
-            std::cout
-                << p->id() << ": "
-                << p->name() << ", "
-                << p->age() << " years old, "
-                << p->height() << "m." << std::endl;
-        }
+      std::cout << "usage: " << std::string(args[0]) << " <numInsert> <inMemory 1|0>" << std::endl;
+      return -1;
     }
+    break;
+  }
+
+  ODBStorage::Ptr storage;
+
+  if (inmemory){
+    storage = std::make_shared<ODBStorage>(":memory:");
+  }
+  else {
+    storage = std::make_shared<ODBStorage>();
+  }
+
+  DBUpdateModule::Ptr updater( new DBUpdateModule(storage) );
+  ActiveObjectStore::Ptr active( new ActiveObjectStore() );
+
+  sempr::core::IDGenerator::getInstance().setStrategy(
+      // std::unique_ptr<sempr::core::UUIDGeneration>( new sempr::core::UUIDGeneration(false) )
+      std::unique_ptr<sempr::core::IncrementalIDGeneration>( new sempr::core::IncrementalIDGeneration(storage) )
+  );
+
+  sempr::core::Core c(storage);
+  c.addModule(active);
+  c.addModule(updater);
+
+  {
+    // insert.
+    for (int i = 0; i < numInsert; i++) {
+        Person::Ptr p(new Person());
+        p->age(0);
+        c.addEntity(p);
+    }
+  }
 
     active->printStats();
 
-    // auto q = std::make_shared<ObjectQuery<Person> >();
-    auto q = std::make_shared<PersonQuery>();
-    c.answerQuery(q);
-    std::cout << "query results: " << q->results.size() << '\n';
-    for (auto p : q->results)
-    {
-        std::cout << p->id() << ", age: " << p->age() << '\n';
+  {
+    std::vector<Person::Ptr> persons;
+    storage->loadAll(persons);
+    for (auto p : persons) {
+        p->loaded(); // no changed-events before announcement!
+        // std::cout << "Person id: " << p->id() << std::endl;
+
+        // add a year.
+        p->age(p->age()+1);
+        std::cout
+            << p->id() << ": "
+            << p->name() << ", "
+            << p->age() << " years old, "
+            << p->height() << "m." << std::endl;
     }
+  }
+
+    active->printStats();
 
     return 0;
 }
