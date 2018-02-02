b0665104c5ccab283b9a458bfaf8de983be092d7
Nils Niemann <nils.niemann@dfki.de>
2018-02-02 15:08:58 +0100

    added RDFDocument: load an RDFEntity from a file

---
+++ src/entity/RDFDocument.cpp
@@ -0,0 +1,68 @@
+#include <sempr/entity/RDFDocument.hpp>
+#include <Soprano/Soprano>
+#include <Soprano/Parser>
+
+#include <RDFDocument_odb.h>
+
+namespace sempr { namespace entity {
+
+RDFDocument::RDFDocument()
+    : RDFEntity(new core::IDGen<RDFDocument>())
+{
+}
+
+RDFDocument::~RDFDocument()
+{
+}
+
+RDFDocument::Ptr RDFDocument::FromFile(const std::string &file)
+{
+    RDFDocument::Ptr doc(new RDFDocument());
+    doc->filename_ = file;
+    
+    //
+    // const Soprano::Parser* p = Soprano::PluginManager::instance()->discoverParserForSerialization( Soprano::SerializationRdfXml );
+    // Soprano::Ser
+
+    Soprano::RdfSerialization options[] = {
+        Soprano::SerializationRdfXml,
+        Soprano::SerializationTurtle,
+        Soprano::SerializationN3,
+        Soprano::SerializationNQuads,
+        Soprano::SerializationNTriples
+    };
+
+    // try different formats for parsing
+    for (auto option : options) {
+        const Soprano::Parser* p = Soprano::PluginManager::instance()->discoverParserForSerialization( option );
+
+        if (!p) {
+            std::cout << "no parser for " << option << '\n';
+            continue;
+        }
+
+        auto statements = p->parseFile(QString::fromStdString(file), QUrl(), option);
+        if (p->lastError() == Soprano::Error::ErrorNone)
+        {
+            while(statements.next())
+            {
+                // qDebug() << *statements;
+                doc->addTriple(
+                    Triple( (*statements).subject().toN3().toStdString(),
+                            (*statements).predicate().toN3().toStdString(),
+                            (*statements).object().toN3().toStdString(),
+                            doc->id() )
+                );
+            }
+
+            break;
+        } else {
+            qDebug() << p->lastError().message();
+        }
+    }
+
+    return doc;
+}
+
+
+}}
--- test/main.cpp
+++ test/main.cpp
@@ -32,6 +32,8 @@ using namespace sempr::query;
 
 #include <sempr/entity/spatial/LocalCS.hpp>
 
+#include <RDFDocument_odb.h>
+
 #ifndef M_PI
 #   define M_PI 3.141592653589793
 #endif
@@ -46,6 +48,10 @@ void print(OGRGeometry* p)
 
 int main(int argc, char** args)
 {
+
+    RDFDocument::FromFile("model.owl");
+
+
     // ODBStorage::Ptr storage( new ODBStorage(":memory:") );
     ODBStorage::Ptr storage( new ODBStorage() );
 
