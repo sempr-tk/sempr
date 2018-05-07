bf02c348c63c5f9ced3ba3d5a0326471a0458f71
Nils <nils.niemann@dfki.de>
2018-05-07 16:38:42 +0200

    replaced Observer::addOverload by Observer::addProcessPair (breaking change)

--- include/sempr/core/Observer.hpp
+++ include/sempr/core/Observer.hpp
@@ -6,46 +6,77 @@
 #include <unordered_map>
 #include <typeindex>
 #include <typeinfo>
+#include <functional>
+#include <vector>
 
 namespace sempr { namespace core {
 
 /**
-    The Observer-Class allows to register functions for specific types of events
-    and queries.
-    E.g.:
-        AddOverload<Observable>(
-            [this](Observable::Ptr foo) { do_something_with(foo); }
-        );
-    Registers a lambda function that takes an Observable::Ptr and calls
-    this->do_something_with(foo) whenever an Observable is to be processed.
-    This can be any type of Event or Query.
-
-    Be aware that this only works for specific types: This function will *not*
-    be called for any derived event/query, only for specific types.
+    The Observer-Class allows to register functions-pairs that operate on Observables. The first
+    function checks if the Observable (Event or Query) shall/can be processed, and the second
+    actually does the processing.
+    The function pairs are encapsuled in a protected struct Observer::ProcessPair. It's main use
+    is in the Module class, which abstracts it away using lambdas and providing virtual methods to
+    be overridden by the user.
 */
 class Observer {
+protected:
+    struct ProcessPair {
+        typedef std::function<bool(Observable::Ptr)> Check_f;
+        typedef std::function<void(Observable::Ptr)> Process_f;
+
+        Check_f checkFunction;
+        Process_f processFunction;
+    };
+
+    /// The list of registered ProcessPairs.
+    std::vector<ProcessPair> processPairs_;
+
+
 public:
     using Ptr = std::shared_ptr<Observer>;
     Observer();
     virtual ~Observer();
 
-    /** Called by the EventBroker or for query-handling **/
+    /**
+        Called by the EventBroker or for query-handling
+
+        This method simply iterates over all registered ProcessPairs and calls all process-methods
+        for which the check-function returned 'true'.
+        TODO: really call *all* of them? Or do we want to start with the last added pair and only call the one with the first successfull check? That way, when a module defines methods to process Geometry *and* Polygon, only the Polygon-one would be called. Well, no need to decide now, this can be added with a flag later on.
+    */
     void notify(Observable::Ptr);
+
+    /**
+        Check if at least one of the ProcessingPairs can process the given Observable.
+    */
+    bool canProcess(Observable::Ptr);
+
+    /**
+        Alias to void notifiy(Observable::Ptr)
+    */
+    void doProcess(Observable::Ptr);
+
 
 protected:
     /** registers a new (type-->function) pair **/
-    template<typename D, typename F>
-    void addOverload(F f) {
-        types_[typeid(D)] =
-            [f](Observable::Ptr o) {
-                f(std::static_pointer_cast<D>(o));
-            };
-    }
+    // template<typename D, typename F>
+    // void addOverload(F f) {
+    //     types_[typeid(D)] =
+    //         [f](Observable::Ptr o) {
+    //             f(std::static_pointer_cast<D>(o));
+    //         };
+    // }
+    void addProcessPair(ProcessPair p);
+    void addProcessPair(ProcessPair::Check_f check, ProcessPair::Process_f process);
+
+
 
 private:
-    std::unordered_map<
-        std::type_index,
-        std::function<void(Observable::Ptr)> > types_;
+    // std::unordered_map<
+    //     std::type_index,
+    //     std::function<void(Observable::Ptr)> > types_;
+
 };
 
 } /* core */
--- src/core/Observer.cpp
+++ src/core/Observer.cpp
@@ -12,13 +12,49 @@ Observer::~Observer()
 }
 
 
+// void Observer::notify(Observable::Ptr o)
+// {
+//     if (types_.find(typeid(*o)) != types_.end()) {
+//         types_[typeid(*o)](o);
+//     } else {
+//         // std::cout << typeid(*this).name() << " couldnt find " << typeid(*o).name() << '\n';
+//     }
+// }
+
 void Observer::notify(Observable::Ptr o)
 {
-    if (types_.find(typeid(*o)) != types_.end()) {
-        types_[typeid(*o)](o);
-    } else {
-        // std::cout << typeid(*this).name() << " couldnt find " << typeid(*o).name() << '\n';
+    doProcess(o);
+}
+
+void Observer::doProcess(Observable::Ptr o)
+{
+    for (auto pair : processPairs_)
+    {
+        if (pair.checkFunction(o)) { pair.processFunction(o); }
     }
+}
+
+bool Observer::canProcess(Observable::Ptr o)
+{
+    for (auto pair : processPairs_)
+    {
+        if (pair.checkFunction(o)) return true;
+    }
+    return false;
+}
+
+void Observer::addProcessPair(ProcessPair p)
+{
+    processPairs_.push_back(p);
+}
+
+void Observer::addProcessPair(ProcessPair::Check_f check, ProcessPair::Process_f process)
+{
+    ProcessPair p;
+    p.checkFunction = check;
+    p.processFunction = process;
+    
+    addProcessPair(p);
 }
 
 } /* core */
