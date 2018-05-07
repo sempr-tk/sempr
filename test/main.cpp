9cac4b32aa6f9b17e00c6c63bb3d9317879ff7af
Nils <nils.niemann@dfki.de>
2018-05-07 15:20:43 +0200

    bad news: include order now matters

--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,7 @@ using namespace sempr::storage;
 #include <sempr/entity/RDFPropertyMap.hpp>
 using namespace sempr::entity;
 
-#include <sempr/processing/DebugModule.hpp>
-#include <sempr/processing/DBUpdateModule.hpp>
-#include <sempr/processing/ActiveObjectStore.hpp>
-#include <sempr/processing/SopranoModule.hpp>
-#include <sempr/processing/SpatialIndex.hpp>
+
 using namespace sempr::processing;
 
 #include <sempr/query/ObjectQuery.hpp>
@@ -38,6 +34,13 @@ using namespace sempr::query;
 
 #include <array>
 
+
+#include <sempr/processing/DebugModule.hpp>
+#include <sempr/processing/DBUpdateModule.hpp>
+#include <sempr/processing/ActiveObjectStore.hpp>
+#include <sempr/processing/SopranoModule.hpp>
+#include <sempr/processing/SpatialIndex.hpp>
+
 #ifndef M_PI
 #   define M_PI 3.141592653589793
 #endif
@@ -49,7 +52,6 @@ void print(OGRGeometry* p)
     std::cout << str << '\n';
     CPLFree(str);
 }
-
 
 
 void setupQuadrangle(OGRPolygon* poly, const std::array<float, 3>& min, const std::array<float, 3>& max)
