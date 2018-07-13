279a17536095fd29bc30b1061a9d0396b6835256
Jaspar Löchte <jstach@uos.de>
2018-07-13 20:51:17 +0200

    Removing old code.

--- include/sempr/storage/traits/traits-sqlite-points.hxx
+++ include/sempr/storage/traits/traits-sqlite-points.hxx
@@ -14,66 +14,6 @@ namespace odb { namespace sqlite {
 
 using sempr::entity::PointCloud;
 
-
-/**
- *  Stores the vectors from the Pointcloud::Points struct into the SQLite DB.
- */
-/*
-template <>
-class value_traits<PointCloud::Points, id_blob>
-{
-public:
-    typedef PointCloud::Points value_type;
-    typedef PointCloud::Points query_type;
-    typedef details::buffer image_type;
-
-    // Make the value from the DB image
-    static void set_value (PointCloud::Points& p, const details::buffer& b, std::size_t n, bool is_null)
-    {
-        if (!is_null)
-        {
-            uint64_t num;
-            uint64_t c_ptr;
-
-            num = n / (sizeof(unsigned char) + sizeof(double));
-            c_ptr = num * sizeof(double);
-
-            p.points = std::vector<double> (b.data(), (b.data() + c_ptr - 1));
-            p.colors = std::vector<unsigned char> ((b.data() + c_ptr), (b.data() + n - 1));
-        }
-        else
-        {
-            // What to do else?
-        }
-    }
-
-    // Create the DB image from the value
-    static void set_image (details::buffer& b, std::size_t& n, bool& is_null,  PointCloud::Points const& p)
-    {
-        uint64_t num;
-        uint64_t c_ptr;
-
-        if (p.points.size() == 0) {
-            is_null = true;
-            n = 0;
-            return;
-        }
-
-        is_null = false;
-
-        num = p.points.size();
-        n = num * (1 + sizeof(double));
-
-        if (n > b.capacity())
-                b.capacity(n);
-        c_ptr = num * sizeof(double);
-
-        std::memcpy(b.data(), p.points.data(), c_ptr);
-        std::memcpy((b.data() + c_ptr), p.colors.data(), num);
-    }
-};
-*/
-
 template <>
 class value_traits<std::vector<PointCloud::Point>, id_blob>
 {
@@ -87,12 +27,11 @@ public:
     {
         if (!is_null)
         {
-            const PointCloud::Point* p = reinterpret_cast<const PointCloud::Point*> (b.data ());
-            v.assign (p, (p + n / sizeof(PointCloud::Point)));
+            const PointCloud::Point* p = reinterpret_cast<const PointCloud::Point*>(b.data());
+            v.assign(p, (p + n / sizeof(PointCloud::Point)));
         }
         else
         {
-            // What to do else?
             v.clear();
         }
     }
@@ -104,14 +43,14 @@ public:
 
         n = v.size() * sizeof(PointCloud::Point);
 
-        if (n > b.capacity())
+        if(n > b.capacity())
         {
             b.capacity(n);
         }
 
         if (n != 0)
         {
-            std::memcpy (b.data(), &v.front(), n);
+            std::memcpy(b.data(), &v.front(), n);
         }
     }
 };
