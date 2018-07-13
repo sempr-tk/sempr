bd6c233f89bab3e1a262244ed6c2c57848f0edd9
Jaspar Löchte <jstach@uos.de>
2018-07-13 20:49:44 +0200

    Correcting errors in Code.

--- include/sempr/entity/PointCloud.hpp
+++ include/sempr/entity/PointCloud.hpp
@@ -26,22 +26,11 @@ namespace sempr { namespace entity {
         PointCloud();
         PointCloud(const sempr::core::IDGenBase*);
 
-        /**
-         * @brief nested struct to store points in vectors
-         */
-        /*
-        struct Points
-        {
-            std::vector<double> points;
-            std::vector<unsigned char> colors;
-        };
-        */
-
         struct Point
         {
             double x, y, z;
             unsigned char r, g, b;
-            // more for hyperspectral
+            // more for hyperspectral, but how?
         };
 
         #pragma db value
@@ -54,33 +43,20 @@ namespace sempr { namespace entity {
 
         
         uint64_t size() const { return m_points.size(); }
-        Bounds bounds() const { return m_bounds; }
-        
-        std::vector<Point> points() { return m_points; }
-        
-        void setPointsWithColor(const std::vector<double> &points, const std::vector<unsigned char> &colors, uint64_t n);
-        void setPoints(std::vector<Point> points) { m_points = points; changed(); }
 
-        void addPoint(Point p) { m_points.push_back(p); changed(); }
-        void removePoint(uint64_t i) { m_points.erase(m_points.begin() + (i - 1)); changed(); }
-
-        /*
-        // return single point i
-        int p(uint64_t i, std::vector<double>& p);
-        // single value of point
-        int x(uint64_t i, double& x) { return  w(i, x, 0); }
-        int y(uint64_t i, double& y) { return  w(i, y, 1); }
-        int z(uint64_t i, double& z) { return  w(i, z, 2); }
-        int w(uint64_t i, double& w, uint64_t j);
+        Bounds& bounds() { return m_bounds; }
+        std::vector<Point>& points() { return m_points; }
+        // TODO: error-handling? i out of bounds?
+        Point& point(int i) { return m_points[i]; }
         
-        // return single color of point i
-        int c(uint64_t i, std::vector<unsigned char>& c);
-        // single color
-        int r(uint64_t i, unsigned char& r) { return  a(i, r, 0); }
-        int g(uint64_t i, unsigned char& g) { return  a(i, g, 1); }
-        int b(uint64_t i, unsigned char& b) { return  a(i, b, 2); }
-        int a(uint64_t i, unsigned char& a, uint64_t j);
-        */
+        void setPointsWithColor(const std::vector<double> &points, const std::vector<unsigned char> &colors);
+        void setPoints(const std::vector<Point>& points) { m_points = points; calculateBounds(); changed(); }
+
+        void addPoint(const Point& p) { checkBounds(p); m_points.push_back(p); changed(); }
+        void removePoint(uint64_t i);
+
+        // these are no save operations.
+        // are these necessary?
         double x(int i) { return m_points[i].x; }
         double y(int i) { return m_points[i].y; }
         double z(int i) { return m_points[i].z; }
@@ -88,28 +64,13 @@ namespace sempr { namespace entity {
         unsigned char g(int i) { return m_points[i].g; }
         unsigned char b(int i) { return m_points[i].b; }
 
-        Point point(int i) { return m_points[i]; }
-
         virtual ~PointCloud() {}
         
     protected:
         friend class odb::access;
         
-        // we should not be able to set the bounds manually - let sempr calculate them TODO
-        void bounds(std::vector<double> b);
-
         void calculateBounds();
-        
-        // how can I make this save?
-        /**
-         * Set the Points
-         */
-        //void points(const std::vector<double>& p) { this->m_points.get()->points = p; this->size(p.size()); }
-        
-        /**
-         * Set the Colors
-         */
-        //void colors(const std::vector<unsigned char>& c) { if(c.size() == 3 * this->size()) this->m_points.get()->colors = c; } // else?
+        inline void checkBounds(const Point& point);
         
         #pragma db type("BLOB")
         // @Blob:   The danger of this kind of operation is that the binary representation
@@ -117,11 +78,9 @@ namespace sempr { namespace entity {
         //          big-endian while another is little-endian; and in case of doubles, even
         //          ABIs with the same endian-ness can have different representations)
         // From:    https://www.codesynthesis.com/pipermail/odb-users/2012-January/000441.html
-        //std::shared_ptr<Points> m_points;
         std::vector<Point> m_points;
+
         Bounds m_bounds;
-        
-        RDFPropertyMap::Ptr prop_;
     };
     
     // enable history:
--- src/entity/PointCloud.cpp
+++ src/entity/PointCloud.cpp
@@ -13,6 +13,7 @@ PointCloud::PointCloud() : PointCloud(new sempr::core::IDGen<PointCloud>())
 
 PointCloud::PointCloud(const sempr::core::IDGenBase* idgen) : sempr::entity::Entity(idgen)
 {
+    // initialize the bounds with zero should be okay ...
     m_bounds.minX = 0.0;
     m_bounds.maxX = 0.0;
     m_bounds.minY = 0.0;
@@ -21,75 +22,50 @@ PointCloud::PointCloud(const sempr::core::IDGenBase* idgen) : sempr::entity::Ent
     m_bounds.maxZ = 0.0;
 }
 
-/*
-int PointCloud::p(uint64_t i, std::vector<double>& p)
-{
-    if (p.size() != 3) // hmm ...
-        p.resize(3);
-    if( (this->x(i, p[0]) == 0) && (this->y(i, p[1]) == 0) && (this->z(i, p[2]) == 0))
-        return 0;
-    return -1;
-}
-
-int PointCloud::w(uint64_t i, double& w, uint64_t j)
-{
-    if (i < this->size())
-    {
-        w = m_points.get()->points[(i * 3) + j];
-        return 0;
-    }
-    return -1;
-}
-
-int PointCloud::c(uint64_t i, std::vector<unsigned char>& c)
-{
-    if (c.size() != 3)
-        c.resize(3);
-    if( (this->r(i, c[0]) == 0) && (this->g(i, c[1]) == 0) && (this->b(i, c[2]) == 0))
-        return 0;
-    return -1;
-}
-
-int PointCloud::a(uint64_t i, unsigned char& a, uint64_t j)
-{
-    // check for m_colors.emtpy() ?
-    // or check for i < m_colors.capacity() ?
-    if (i < this->size())
-    {
-        a = m_points.get()->colors[(i * 3) + j];
-        return 0;
-    }
-    return -1;
-}
-*/
-
 void PointCloud::calculateBounds()
 {
+    // make sure, that we get the right bounds
+    m_bounds.minX = m_bounds.maxX = m_points[0].x;
+    m_bounds.minY = m_bounds.maxY = m_points[0].y;
+    m_bounds.minZ = m_bounds.maxZ = m_points[0].z;
+
     for(auto const& point : m_points)
     {
-        if(point.x < m_bounds.minX) m_bounds.minX = point.x;
-        if(point.x > m_bounds.maxX) m_bounds.maxX = point.x;
-        if(point.y < m_bounds.minY) m_bounds.minY = point.y;
-        if(point.y > m_bounds.maxY) m_bounds.maxY = point.y;
-        if(point.z < m_bounds.minZ) m_bounds.minZ = point.z;
-        if(point.z > m_bounds.maxZ) m_bounds.maxZ = point.z;
+        checkBounds(point);
     }
+}
+
+inline void PointCloud::checkBounds(const Point &point) {
+    if(point.x < m_bounds.minX) m_bounds.minX = point.x;
+    if(point.x > m_bounds.maxX) m_bounds.maxX = point.x;
+    if(point.y < m_bounds.minY) m_bounds.minY = point.y;
+    if(point.y > m_bounds.maxY) m_bounds.maxY = point.y;
+    if(point.z < m_bounds.minZ) m_bounds.minZ = point.z;
+    if(point.z > m_bounds.maxZ) m_bounds.maxZ = point.z;
+}
+
+void PointCloud::removePoint(uint64_t i)
+{
+    Point& p = m_points[i];
+    if(p.x == m_bounds.minX || p.x == m_bounds.maxX ||
+       p.y == m_bounds.minY || p.y == m_bounds.maxY ||
+       p.z == m_bounds.minZ || p.z == m_bounds.maxZ)
+    {
+        calculateBounds();
+    }
+
+    m_points.erase(m_points.begin() + i);
+    changed();
 }
 
 void PointCloud::setPointsWithColor(std::vector<double> const& points,
-                                    std::vector<unsigned char> const& colors,
-                                    uint64_t n)
+                                    std::vector<unsigned char> const& colors)
 {
     uint64_t k = 0;
-    std::cout << "in" << std::endl;
-
-    //static_assert(points.size() == (3 * n), "Too much or not enough point values");
-    //static_assert(colors.size() == (3 * n), "Too much or not enough color values");
 
     // does it make sence to check if there are allready points?
-    this->m_points = std::vector<Point>(n);
+    m_points = std::vector<Point>(points.size() / 3);
 
-    //for(std::vector<Point>::iterator it = m_points.begin(); it != )
     for(auto & point : m_points)
     {
         // calculate the points here?!
@@ -101,17 +77,8 @@ void PointCloud::setPointsWithColor(std::vector<double> const& points,
         point.b = colors[k++];
     }
 
-    // we need to trust the input? :(
-    // maybe think about some saver method to get the points (and the number)
-    //this->points(points);
-    //this->colors(colors);
     calculateBounds();
-
     changed();
-
-    std::cout << "process finished" << std::endl;
-    std::cout << "Testpoint: " << this->m_points[n/3].x << std::endl;
-
 }
 
 }}
