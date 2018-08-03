84f70fd88a5a8865064e3005c0ea7acf1a7c8241
ctieben <ctieben@gmx.de>
2018-08-03 16:08:56 +0100

    Add a simple setter for the shell of a polygon.

--- include/sempr/entity/spatial/Polygon.hpp
+++ include/sempr/entity/spatial/Polygon.hpp
@@ -26,6 +26,13 @@ public:
     void setGeometry(geom::Polygon* geometry);
 
     /**
+     * @brief Set the shell coordindates. Will overwrite the polygon geometry (including the holes)!
+     * 
+     * @note the coordinates have to define a closed linear ring. so the first and last coordinates have to be equal.
+     */
+    void setCoordinates(std::vector<geom::Coordinate>& coordinates); //no const parameter because of geos::geom!
+
+    /**
         Get a new entity with the same geometry (copy) referring to the same instance of
         SpatialReference.
     */
