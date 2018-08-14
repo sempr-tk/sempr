04c01c483123a2ce9f3a942d01fda642742885b0
Christoph Tieben <ctieben@gmx.de>
2018-08-14 16:13:15 +0200

    First draft of renew PointCloud

--- src/entity/spatial/PointCloud.cpp
+++ src/entity/spatial/PointCloud.cpp
@@ -7,32 +7,104 @@ namespace sempr { namespace entity {
 
 SEMPR_ENTITY_SOURCE(PointCloud)
 
-PointCloud::PointCloud() : PointCloud(new sempr::core::IDGen<PointCloud>())
+
+
+CoordinatePoint::CoordinatePoint(const geom::Coordinate& coord) :
+    coord_(coord)
+{
+
+}
+/*
+double& CoordinatePoint::operator[](std::size_t idx)
+{
+    if (idx == 0)
+        return coord_.x;
+    else if (idx == 1)
+        return coord_.y;
+    else if (idx == 2)
+        return coord_.z;
+    else
+        throw std::out_of_range(); // Out of boundary!
+
+    return coord_.z;
+}
+*/
+const double& CoordinatePoint::operator[](std::size_t idx) const
+{
+    if (idx == 0)
+        return coord_.x;
+    else if (idx == 1)
+        return coord_.y;
+    else if (idx == 2)
+        return coord_.z;
+    else
+        throw std::out_of_range(""); // Out of boundary!
+
+    return coord_.z;
+}
+
+
+PointCloud::PointCloud() : 
+    PointCloud(new sempr::core::IDGen<PointCloud>())
 {
 }
 
-PointCloud::PointCloud(const sempr::core::IDGenBase* idgen) : sempr::entity::MultiPoint(idgen)
+PointCloud::PointCloud(const sempr::core::IDGenBase* idgen) : 
+    sempr::entity::MultiPoint(idgen)
 {
     setDiscriminator<PointCloud>();
 
 }
 
-
-/*
-void PointCloud::removePoint(uint64_t i)
+PointCloud::~PointCloud()
 {
-    // TODO
-    changed();
+
 }
-*/
 
-void PointCloud::setCoordinatesWithColor(const std::vector<geom::Coordinate> &coordinates,
-                                    std::vector<unsigned char> const& colors)
+
+bool PointCloud::hasChannel(int type) const
 {
-    setCoordinates(coordinates);
+    auto it = channels_.find(type);
 
-    //Color is todo.
-    changed();
+    return it != channels_.end();
+}
+
+void PointCloud::setChannel(int type, const std::vector<double>& channel)
+{
+    if(channel.size() != size())
+        throw std::exception(); // no equial size of points and channel information
+
+    channels_[type] = Channel(channel);
+}
+
+std::vector<double>& PointCloud::getChannel(int type)
+{
+    if (!hasChannel(type))
+        throw std::out_of_range("");
+
+    return channels_[type].channel_;
+}
+/*
+const std::vector<double>& PointCloud::getChannel(int type) const
+{
+    if (hasChannel(type))
+        return channels_[type].channel_;
+
+    else
+        throw std::out_of_range("");
+
+    return std::vector<double>();
+}*/
+
+std::size_t PointCloud::size() const
+{
+    return getGeometry()->getNumGeometries(); // each point is a geometry so the num of geometries shall be equal to the num of points!
+}
+
+
+const AbstractPoint& PointCloud::operator[](std::size_t idx) const
+{
+    return CoordinatePoint(*getGeometry()->getGeometryN(idx)->getCoordinate());  //implicit type cast
 }
 
 }}
