ca8b646bdacaaf0ff0d4c645495f7c069287d3c8
Nils Niemann <nils.niemann@dfki.de>
2020-03-17 12:18:54 +0100

    removed odb

--- include/odb_storage/traits/traits-sqlite.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef SEMPR_TRAITS_SQLITE_HXX
-#define SEMPR_TRAITS_SQLITE_HXX
-
-#include <sempr/storage/traits/traits-sqlite-uuid.hxx>
-#include <sempr/storage/traits/traits-sqlite-geom-geometry.hxx>
-#include <sempr/storage/traits/traits-sqlite-pointcloud.hxx>
-#include <sempr/storage/traits/traits-sqlite-eigen.hxx>
-
-#endif /* end of include guard: SEMPR_TRAITS_SQLITE_HXX */
