a9fa9bb1a744f4e1b2a6fb6e5ea3e1315885a043
Juan Carlos Saborio <juan.saborio@dfki.de>
2021-05-07 13:41:30 +0200

    Updated Date Builders

--- src/nodes/DateDayBuilder.cpp
+++ src/nodes/DateDayBuilder.cpp
@@ -15,41 +15,10 @@ DateDayBuilder::DateDayBuilder()
 rete::Builtin::Ptr DateDayBuilder::buildBuiltin(rete::ArgumentList& args) const
 {
     // exactly 2 args: day, date
-    if (args.size() != 2) throw rete::NodeBuilderException("Invalid number of arguments (!=2)");
-    if (args[0].isConst() || args[0].getAccessor()) throw rete::NodeBuilderException("First argument must be unbound, as it is reserved for the result.");
+    rete::util::requireNumberOfArgs(args, 2);
+    rete::util::requireUnboundVariable(args, 0);
 
-    rete::PersistentInterpretation<std::string> date;
-    if (args[1].isConst())
-    {
-        if (args[1].getAST().isString())
-        {
-            rete::ConstantAccessor<std::string> acc(args[1].getAST().toString());
-            acc.index() = 0;
-            date = acc.getInterpretation<std::string>()->makePersistent();
-        }
-        else
-        {
-            throw rete::NodeBuilderException("Date parameter is not a string");
-        }
-    }
-    else /* isVariable */
-    {
-        auto acc = args[1].getAccessor();
-        if (acc)
-        {
-            auto strInterp = acc->getInterpretation<std::string>();
-            if (strInterp)
-                date = strInterp->makePersistent();
-            else
-                throw rete::NodeBuilderException(
-                        "Variable " + args[1].getVariableName() + " is not a string");
-        }
-        else
-        {
-            throw rete::NodeBuilderException(
-                    "Variable " + args[1].getVariableName() + " is unbound. Must be a string.");
-        }
-    }
+    auto date = rete::util::requireInterpretation<std::string>(args, 1);
     
     // create the node
     auto node = std::make_shared<DateDayNode>(std::move(date));
--- src/nodes/DateDiffBuilder.cpp
+++ src/nodes/DateDiffBuilder.cpp
@@ -15,75 +15,11 @@ DateDiffBuilder::DateDiffBuilder()
 rete::Builtin::Ptr DateDiffBuilder::buildBuiltin(rete::ArgumentList& args) const
 {
     // exactly 3 args: diff, date1, date2
-    if (args.size() != 3) throw rete::NodeBuilderException("Invalid number of arguments (!=3)");
-    if (args[0].isConst() || args[0].getAccessor()) throw rete::NodeBuilderException("First argument must be unbound, as it is reserved for the result.");
+    rete::util::requireNumberOfArgs(args, 3);
+    rete::util::requireUnboundVariable(args, 0);
 
-    rete::PersistentInterpretation<std::string> date1;
-    rete::PersistentInterpretation<std::string> date2;
-
-    if (args[1].isConst())
-    {
-        if (args[1].getAST().isString())
-        {
-            rete::ConstantAccessor<std::string> acc(args[1].getAST().toString());
-            acc.index() = 0;
-            date1 = acc.getInterpretation<std::string>()->makePersistent();
-        }
-        else
-        {
-            throw rete::NodeBuilderException("Second parameter is not a string");
-        }
-    }
-    else /* isVariable */
-    {
-        auto acc = args[1].getAccessor();
-        if (acc)
-        {
-            auto strInterp = acc->getInterpretation<std::string>();
-            if (strInterp)
-                date1 = strInterp->makePersistent();
-            else
-                throw rete::NodeBuilderException(
-                        "Variable " + args[1].getVariableName() + " is not a string");
-        }
-        else
-        {
-            throw rete::NodeBuilderException(
-                    "Variable " + args[1].getVariableName() + " is unbound. Must be a string.");
-        }
-    }
-    
-    if (args[2].isConst())
-    {
-        if (args[2].getAST().isString())
-        {
-            rete::ConstantAccessor<std::string> acc(args[2].getAST().toString());
-            acc.index() = 0;
-            date2 = acc.getInterpretation<std::string>()->makePersistent();
-        }
-        else
-        {
-            throw rete::NodeBuilderException("Third parameter is not a string");
-        }
-    }
-    else
-    {
-        auto acc2 = args[2].getAccessor();
-        if (acc2)
-        {
-            auto strInterp = acc2->getInterpretation<std::string>();
-            if (strInterp)
-                date2 = strInterp->makePersistent();
-            else
-                throw rete::NodeBuilderException(
-                        "Variable " + args[2].getVariableName() + " is not a string");
-        }
-        else
-        {
-            throw rete::NodeBuilderException(
-                    "Variable " + args[2].getVariableName() + " is unbound. Must be a string.");
-        }
-    }   
+    auto date1 = rete::util::requireInterpretation<std::string>(args, 1);
+    auto date2 = rete::util::requireInterpretation<std::string>(args, 2); 
     
     // create the node
     auto node = std::make_shared<DateDiffNode>(std::move(date1), std::move(date2));
